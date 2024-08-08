6a3bb23995ffb32c51fa900ffad9aba17f554690
Lila <lilalovesall@gmail.com>
2024-08-08 17:09:01 +0100

    base

---
+++ levels/bob/header.inc.h
@@ -0,0 +1,16 @@
+extern const GeoLayout bob_area_1_geo[];
+extern const GeoLayout bob_area_1[];
+extern const Collision bob_area_1_collision[];
+extern const MacroObject bob_area_1_macro_objs[];
+extern Lights1 bob_dl_Grass_Example_004_lights;
+extern u8 bob_dl_i8_i8[];
+extern Vtx bob_dl_Level_Mesh_mesh_layer_1_vtx_0[4];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_1_tri_0[];
+extern Vtx bob_dl_Level_Mesh_mesh_layer_5_vtx_0[4];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_5_tri_0[];
+extern Gfx mat_bob_dl_Grass_Example_004[];
+extern Gfx mat_revert_bob_dl_Grass_Example_004[];
+extern Gfx mat_bob_dl_f3dlite_material[];
+extern Gfx mat_revert_bob_dl_f3dlite_material[];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_1[];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_5[];
